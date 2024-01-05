2c03d0fb80c0d5e96df4522a1bca67330163e072
IgorZvezdilin <zvezdilin.work@mail.ru>
2024-01-05 18:44:18 +0300

    Add authentication using clerk

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,12 @@
+import { UserButton } from "@clerk/nextjs"
+
 export default function Home() {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1 className=' h1-bold '>Hello Next js</h1>
+        <div>
+            <UserButton afterSignOutUrl="/"/>
+        </div>
       </main>
     )
   }
